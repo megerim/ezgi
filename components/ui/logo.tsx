14983bcf356652a41623679b5877726b7cec4944
Mehmet Erim Gökhan <mehmeterimgokhan99@gmail.com>
2024-01-22 02:22:28 +0300

    logoFix

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,14 +1,13 @@
 import Link from 'next/link';
 import Image from 'next/image'
-import LogoFoto from '@/public/images/logo.png'
 
 export default function Logo() {
   return (
     <Link href="/" className="block" aria-label="Ezgi">
       
-      <div className="">
+      <div>
       <Image 
-        src={LogoFoto}
+        src={"/images/logo.png"}
         alt="Logo"
         width={1950}
         height={1250}
