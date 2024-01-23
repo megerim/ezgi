c8bf08bda3b146922d74aafa9f7d4d2d3efa1dbf
Mehmet Erim Gökhan <mehmeterimgokhan99@gmail.com>
2024-01-23 03:14:21 +0300

    opt

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -33,15 +33,15 @@ export async function POST(req: NextRequest) {
     from: formData.email,
     to: "merimgokhan@gmail.com", // Replace with the appropriate recipient email
     subject: "Yeni Randevu Talebi",
-    text: `Name: ${formData.name}\nSurname: ${formData.surname}\nEmail: ${formData.email}\nPhone: ${formData.phone}\nPreferred Contact Method: ${formData.contactPreference}\nDate: ${formData.date}\nMeeting Type: ${formData.meetingType}\nMessage: ${formData.message}`,
+    text: `İsim: ${formData.name}\nSoyisim: ${formData.surname}\nEmail: ${formData.email}\nTelefon: ${formData.phone}\nİletişim Yöntemi: ${formData.contactPreference}\nTarih: ${formData.date}\nRandevu Türü: ${formData.meetingType}\nMessage: ${formData.message}`,
     html: `<div>
-             <p><b>Name:</b> ${formData.name}</p>
-             <p><b>Surname:</b> ${formData.surname}</p>
+             <p><b>İsim:</b> ${formData.name}</p>
+             <p><b>Soyisim:</b> ${formData.surname}</p>
              <p><b>Email:</b> ${formData.email}</p>
-             <p><b>Phone:</b> ${formData.phone}</p>
-             <p><b>Preferred Contact Method:</b> ${formData.contactPreference}</p>
-             <p><b>Date:</b> ${formData.date}</p>
-             <p><b>Meeting Type:</b> ${formData.meetingType}</p>
+             <p><b>Telefon:</b> ${formData.phone}</p>
+             <p><b>İletişim Yöntemi:</b> ${formData.contactPreference}</p>
+             <p><b>Tarih:</b> ${formData.date}</p>
+             <p><b>Randevu Türü:</b> ${formData.meetingType}</p>
              <p><b>Message:</b> ${formData.message}</p>
            </div>`,
   };
@@ -50,13 +50,13 @@ export async function POST(req: NextRequest) {
   try {
     const result = await transporter.sendMail(mailOptions);
     return NextResponse.json(
-      { success: true, message: 'Email sent', result },
+      { success: true, message: 'Email gönderildi', result },
       { status: 200 }
     );
   } catch (error: any) {
     console.error("Error sending email:", error);
     return NextResponse.json(
-      { success: false, message: 'Failed to send email', errorDetails: error.message },
+      { success: false, message: 'HATA', errorDetails: error.message },
       { status: 500 }
     );
   }
