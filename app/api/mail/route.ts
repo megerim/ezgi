4316fdcf7f07a8d91aa95fbe9d0bcb545711b903
Mehmet Erim Gökhan <mehmeterimgokhan99@gmail.com>
2024-01-23 03:10:58 +0300

    mail optimization

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -31,11 +31,21 @@ export async function POST(req: NextRequest) {
 
   const mailOptions = {
     from: formData.email,
-    to: "merimgokhan@gmail.com", // Replace this with the appropriate recipient email
+    to: "merimgokhan@gmail.com", // Replace with the appropriate recipient email
     subject: "Yeni Randevu Talebi",
-    text: formData.message,
-    html: `<div>${formData.message}</div>`,
+    text: `Name: ${formData.name}\nSurname: ${formData.surname}\nEmail: ${formData.email}\nPhone: ${formData.phone}\nPreferred Contact Method: ${formData.contactPreference}\nDate: ${formData.date}\nMeeting Type: ${formData.meetingType}\nMessage: ${formData.message}`,
+    html: `<div>
+             <p><b>Name:</b> ${formData.name}</p>
+             <p><b>Surname:</b> ${formData.surname}</p>
+             <p><b>Email:</b> ${formData.email}</p>
+             <p><b>Phone:</b> ${formData.phone}</p>
+             <p><b>Preferred Contact Method:</b> ${formData.contactPreference}</p>
+             <p><b>Date:</b> ${formData.date}</p>
+             <p><b>Meeting Type:</b> ${formData.meetingType}</p>
+             <p><b>Message:</b> ${formData.message}</p>
+           </div>`,
   };
+  
 
   try {
     const result = await transporter.sendMail(mailOptions);
