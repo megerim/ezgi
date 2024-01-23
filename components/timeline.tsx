32fbc0a44f8ca3f36b11eeb47348f6700af0c734
Mehmet Erim Gökhan <mehmeterimgokhan99@gmail.com>
2024-01-23 03:56:16 +0300

    textcolor

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -17,7 +17,7 @@ const TimelineItem: React.FC<TimelineItemProps> = ({ date, title, description, a
     <div className="order-1 w-5/12"></div>
     <div className={`order-1 w-5/12 px-1 py-4 ${alignment === 'left' ? 'text-right' : 'text-left'}`}>
       <p className="mb-3 text-base text-third">{date}</p>
-      <h4 className="mb-3 font-bold text-lg md:text-2xl">{title}</h4>
+      <h4 className="mb-3 font-bold text-lg md:text-2xl text-secondary">{title}</h4>
       <p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">{description}</p>
     </div>
   </div>
